603f3a8f3cac351f1fc2000ebb0e67ab55bc8642
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-10-09 18:13:03 -0300

    refactor: update Contest factory with proper lottery game names

--- database/factories/ContestFactory.php
+++ database/factories/ContestFactory.php
@@ -23,19 +23,11 @@ class ContestFactory extends Factory
     public function definition(): array
     {
         return [
-            'lottery_game_id'           => LotteryGame::factory(),
-            'draw_number'               => $this->faker->numberBetween(1000, 9999),
-            'draw_date'                 => $this->faker->date(),
-            'location'                  => $this->faker->city() . ', ' . $this->faker->stateAbbr(),
-            'numbers'                   => $this->faker->randomElements(range(1, 60), 6),
-            'has_accumulated'           => $this->faker->boolean(),
-            'next_draw_number'          => $this->faker->numberBetween(1000, 9999),
-            'next_draw_date'            => $this->faker->dateTimeBetween('now', '+1 week')->format('Y-m-d'),
-            'estimated_prize_next_draw' => $this->faker->randomFloat(2, 1000000, 50000000),
-            'extra_data'                => [
-                'observacao'      => $this->faker->sentence(),
-                'valorArrecadado' => $this->faker->randomFloat(2, 10000000, 100000000),
-            ],
+            'lottery_game_id' => LotteryGame::factory(),
+            'draw_number'     => $this->faker->numberBetween(1000, 9999),
+            'draw_date'       => $this->faker->date(),
+            'location'        => $this->faker->city() . ', ' . $this->faker->stateAbbr(),
+            'numbers'         => $this->faker->randomElements(range(1, 60), 6),
         ];
     }
 }
