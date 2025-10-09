cd9975087d97fa360be8201cccff33bd910e24d0
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-10-09 19:12:21 -0300

    feat: add public API routes for contest data and intelligent game generation

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\{ContestController, GameGeneratorController};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Rotas públicas para concursos
+Route::prefix('contests')->group(function () {
+    Route::get('/latest', [ContestController::class, 'latestAll']);
+    Route::get('/latest/{gameSlug}', [ContestController::class, 'latest']);
+});
+
+// Rotas públicas para geração de jogos
+Route::prefix('games')->group(function () {
+    Route::get('/info', [GameGeneratorController::class, 'info']);
+    Route::get('/session-stats', [GameGeneratorController::class, 'sessionStats']);
+    Route::post('/generate/{gameSlug}', [GameGeneratorController::class, 'generate']);
+});
